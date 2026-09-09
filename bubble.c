dbcc2ab8c2d6d10349c4419025c1e94245ee39b2
Ari-Leger <ari.leger@bsu.edu>
2026-09-09 17:33:10 +0000

    adding in the code of the sorter

---
+++ bubble.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+const int MAX = 9;
+
+void printValues(int*);
+void sort(int*);
+void swap(int*, int*);
+
+int main() {
+    int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+    printf("Before:\n");
+    printValues(values);
+
+    // test swap
+    int x = 3;
+    int y = 5;
+    printf("x: %d, y: %d\n", x, y);
+    swap(&x, &y);
+    printf("x: %d, y: %d\n", x, y);
+
+    sort(values);
+    printf("After:\n");
+    printValues(values);
+
+    return 0;
+}
+
+// print array values
+void printValues(int* array) {
+    printf("[");
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("]\n");
+}
+
+// swap two integers using pointers
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// bubble sort with print after each swap
+void sort(int* array) {
+    int i, j;
+
+    for (i = 0; i < MAX - 1; i++) {
+        for (j = 0; j < MAX - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+                printValues(array);
+            }
+        }
+    }
+}
+// end main
